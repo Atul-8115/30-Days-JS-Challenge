1a0f405f2e4110921d66a9442d9d0c24121a69ce
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-17 12:40:16 +0530

    completed Activity 1 of Day 2

---
+++ Day_2_Operators/Activity_1.js
@@ -0,0 +1,24 @@
+// ************ Arithmetic Operators ************
+
+// Task 1:
+
+let num1 = 5
+let num2 = 6
+const sum = num1 + num2
+console.log(sum)
+
+// Task 2:
+
+const a = 7
+const b = 3
+const c = a - b
+console.log(c)
+
+// Task 3:
+
+let multiply = num1 * num2
+console.log(multiply)
+
+let modulo = 7 % 3
+console.log(modulo)
+
