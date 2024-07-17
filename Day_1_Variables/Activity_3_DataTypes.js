beec48d9c1d17565f391ee125967525739f301c6
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-17 12:23:06 +0530

    Completed Activity 3 Data Types

---
+++ Day_1_Variables/Activity_3_DataTypes.js
@@ -0,0 +1,13 @@
+// Task 4:
+
+let number = 5
+let Name = "Atul"
+let bool = true
+let arr = []
+let object = {"name": "Atul"}
+
+console.log(typeof number)
+console.log(typeof Name)
+console.log(typeof bool)
+console.log(typeof arr) // Gives type as object
+console.log(typeof object)
