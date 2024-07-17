dae99519bcdf4bbbf01549f3092362c585dbf47f
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-17 12:28:59 +0530

    Completed Activity 4 and 5

---
+++ Day_1_Variables/Activity_4.js
@@ -0,0 +1,15 @@
+// Reassigning Variables
+
+// Task 5:
+
+let variable = "hello"
+console.log(variable)
+
+variable = "hi"
+console.log(variable)
+
+const contant = 5
+console.log(contant)
+
+contant = 6
+console.log(contant)
