e22e426d6cae8dd0274eeded6d2c3dbae572ed90
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-17 12:52:22 +0530

    Completed Activity 3 of Day 2

---
+++ Day_2_Operators/Activity_3.js
@@ -0,0 +1,36 @@
+// ************ Comparision Operators ************
+
+// Task 8:
+
+let num1 = 10
+let num2 = 12
+
+console.log(num2 > num1)
+
+console.log(num2 < num1)
+
+// Task 9:
+if(num1 >= num2) {
+    console.log("Number 1 is greater than or equal to number2")
+} else {
+    console.log("Number 1 is less than Number 2")
+}
+
+let num3 = 5
+let num4 = 2
+
+if(num4 <= num3) {
+    console.log("num4 is less than num3")
+} else {
+    console.log("num4 is greater than num3")
+}
+
+// Task 10:
+let num5 = 10
+let num6 = "10"
+
+let result = num5 == num6
+console.log(result)
+
+result = num5 === num6
+console.log(result)
