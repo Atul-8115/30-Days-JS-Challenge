f740c2282ddbf7b55e86def3ebcab81d4e1850cf
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-17 12:57:38 +0530

    Completed Activity 4 of Day 2

---
+++ Day_2_Operators/Activity_4.js
@@ -0,0 +1,19 @@
+// ************ Logical Operators ************
+
+// Task 11:
+
+let num1 = 5
+let num2 = 7
+let num3 = 7
+let num4 = 7
+
+if(num1 < num2 && num3 === num4) {
+    console.log("Exprementing with and operator")
+}
+if(num1 < num2 || num3 === num4) {
+    console.log("Exprementing with or operator")
+}
+
+let bool = true
+
+console.log(!bool)
