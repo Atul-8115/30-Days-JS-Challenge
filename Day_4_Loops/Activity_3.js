51fbe3e780a7e2c3a143cfcdcb19dd05d957bf95
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-18 19:30:27 +0530

    Completed Activity 3 of Day 4

---
+++ Day_4_Loops/Activity_3.js
@@ -0,0 +1,19 @@
+// *************** Do....While Loop ***************
+
+// Task 5:
+
+let i = 1
+do {
+    console.log(i)
+    i++
+} while (i <= 5);
+
+// Task 6:
+let num = 4
+let fact = 1
+do {
+    fact *= num
+    num--
+} while(num > 0)
+
+console.log("Factorial of the number is => ",fact)
