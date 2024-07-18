2ab8bd5302c861522ff714b9165aef4095635426
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-18 11:21:26 +0530

    Completed Activity 1 of Day 3

---
+++ Day_3_Control_Structures/Activity_1.js
@@ -0,0 +1,23 @@
+// ************* if-else statements
+
+// Task 1:
+
+let num = -2
+
+if(num > 0) {
+    console.log("Number is positive")
+} else if(num == 0) {
+    console.log("Number is zero")
+} else {
+    console.log("Number is negative")
+}
+
+// Task 2:
+
+let age = 35
+
+if(age >= 18) {
+    console.log("Person is elegible to vote")
+} else {
+    console.log("Person is not elegible to vote.")
+}
