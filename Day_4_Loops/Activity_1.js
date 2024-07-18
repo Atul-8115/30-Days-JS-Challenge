3f219d57130464a0c1780cee016f5656fbb38ee1
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-18 19:19:52 +0530

    Completed Activity 2 of Day 4

---
+++ Day_4_Loops/Activity_1.js
@@ -0,0 +1,12 @@
+// *************** For Loop ***************
+
+// Task 1:
+let n = 10;
+for (let i = 0; i < n; i++) {
+    // console.log(i+1)
+}
+
+// Task 2:
+for(let i = 0;i < n;i++) {
+    console.log(`5 * ${i+1} => `,5 * (i+1))
+}
