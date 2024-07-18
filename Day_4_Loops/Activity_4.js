5d80c15b750eaa257857c319738c0f5394a25a7a
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-18 19:43:59 +0530

    Completed Activity 4 of Day 4

---
+++ Day_4_Loops/Activity_4.js
@@ -0,0 +1,10 @@
+// *************** Nested Loops ***************
+
+// Task 7:
+for(let i = 0; i < 5; i++) {
+    let str = ""
+    for(let j = 0; j < i+1; j++) {
+        str += "* "
+    }
+    console.log(str)
+}
