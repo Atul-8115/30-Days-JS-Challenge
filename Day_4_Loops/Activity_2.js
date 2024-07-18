2927cf0627a8f763f78509dea973faa014979847
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-18 19:24:48 +0530

    Completed Activity 2 of Day 4

---
+++ Day_4_Loops/Activity_2.js
@@ -0,0 +1,19 @@
+// *************** While Loop ***************
+
+// Task 3:
+let i = 1
+let sum = 0;
+
+while(i <= 10) {
+    sum += i
+    i++
+}
+
+console.log("Printing sum from 1 to 10 => ",sum)
+
+// Task 4:
+i = 10;
+while(i > 0) {
+    console.log(i)
+    i--
+}
