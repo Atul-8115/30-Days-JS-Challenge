16eb696bb3da00571ddbe064dc9f79c2d9b884d6
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-21 21:17:27 +0530

    Completed Activity 5 of Day 6

---
+++ Day_6_Arrays/Activity_5.js
@@ -0,0 +1,14 @@
+// **************** Multi-dimensional Arrays ****************
+
+// Task 12:
+let arr = [[1,2,3],[4,5,6],[7,8,9]]
+
+for(let i = 0;i < arr.length; i++) {
+    console.log(arr[i]) 
+    // for(let j = 0; j < arr[i].length; j++) {
+    //     console.log(`arr[${i}][${j}] -> `,arr[i][j])
+    // }
+}
+
+// Task 13:
+console.log("Accessing a specific element -> ",arr[2][0])
