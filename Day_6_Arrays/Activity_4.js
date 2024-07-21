d499a73199d12fbd0f68095eb5edeb22bcfddc1a
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-21 21:05:52 +0530

    Completed Activity 4 of Day 6

---
+++ Day_6_Arrays/Activity_4.js
@@ -0,0 +1,14 @@
+// **************** Array Iteration ****************
+
+// Task 10:
+
+let arr = [1,2,3,4,5,6,7,8,9,10] 
+
+for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
+
+// Task 11:
+
+// Having not any return value
+arr.forEach(ele => console.log(ele))
