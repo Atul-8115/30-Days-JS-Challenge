3cf2bdb7d0a4184d825b6273f9626b2d18c67766
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-21 20:40:48 +0530

    Completed Activity 2 of Day 6

---
+++ Day_6_Arrays/Activity_2.js
@@ -0,0 +1,22 @@
+// **************** Array Methoda(Basics) ****************
+
+// Task 3:
+let arr = [1,2,3,4]
+
+console.log("Array before pushing the data")
+console.log(arr)
+arr.push(5)
+console.log("Array after pushing the data")
+console.log(arr)
+
+// Task 4:
+arr.pop()
+console.log("After deleting last element -> ",arr)
+
+// Task 5:
+let ele = arr.shift()
+console.log("After performing shift operation the array -> ",arr," and the element is -> ",ele)
+
+// Task 6:
+arr.unshift(0)
+console.log("After unshift operation -> ",arr)
