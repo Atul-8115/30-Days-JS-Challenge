4cdf57dfee43740b89a515c7d8f519aef550cd66
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-22 08:34:45 +0530

    Tried Another method of accessing object's value

--- Day_7_Objects/Activity_1.js
+++ Day_7_Objects/Activity_1.js
@@ -2,11 +2,12 @@
 
 // Task 1:
 
-const book = {"title": "The Atomic Habit", "author": "Charles Duhigg", "year": "1998"}
+const book = {title: "The Atomic Habit", author: "Charles Duhigg", year: "1998"}
 
 console.log(book)
 
 // Task 2:
 
 console.log("Title of the book -> ",book.title)
+console.log("Title of the book -> ",book["title"])
 console.log("Author of this book is -> ",book.author)
