80ddf04f2505815e4bf318b1c3f81e840ebfec59
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-22 08:11:35 +0530

    Completed Activity 1 of Day 7

---
+++ Day_7_Objects/Activity_1.js
@@ -0,0 +1,12 @@
+// **************** Object Creation and Access ****************
+
+// Task 1:
+
+const book = {"title": "The Atomic Habit", "author": "Charles Duhigg", "year": "1998"}
+
+console.log(book)
+
+// Task 2:
+
+console.log("Title of the book -> ",book.title)
+console.log("Author of this book is -> ",book.author)
