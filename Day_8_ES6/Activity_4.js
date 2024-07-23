4a9af2311d25d4854c9fb2754f6f0d11ccde9056
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-23 20:43:30 +0530

    Completed Activity 4 of Day 8

---
+++ Day_8_ES6/Activity_4.js
@@ -0,0 +1,10 @@
+// *************** Default Parameters ***************
+
+// Task 7:
+
+function product (num1, num2 = 1) {
+    return num1 * num2
+}
+
+console.log(product(5,10))
+console.log(product(7))
