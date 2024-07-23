01e89c3d0f20d44084e05b79aa1dd11de59a912e
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-07-23 20:13:50 +0530

    Completed Activity 1 of Day 8

---
+++ Day_8_ES6/Activity_1.js
@@ -0,0 +1,16 @@
+// *************** Template Literals ***************
+
+// Task 1:
+let name = "Atul"
+let age = 20
+
+let str1 = `Hello ${name} your are now ${age} years old`
+
+console.log(str1)
+
+// Task 2:
+let str2 = `Dear HR Manger,
+I'm Atul Kumar Pandey
+I'm here for asking referral to you`
+
+console.log(str2)
